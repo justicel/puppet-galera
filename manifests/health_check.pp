f8280148d70ee7bf550eeef724bad0e3af3c8ea5
Justice London <jlondon@syrussystems.com>
2012-12-27 10:03:04 -0800

    Changed health check port to 9220 to avoid conflicts with new 'services' definitions.

--- manifests/health_check.pp
+++ manifests/health_check.pp
@@ -11,9 +11,9 @@
 #  mode  tcp
 #  option  tcpka
 #  option  httpchk
-#  server  control01 192.168.220.41:3306 check port 9200 inter 2000 rise 2 fall 5
-#  server  control02 192.168.220.42:3306 check port 9200 inter 2000 rise 2 fall 5
-#  server  control03 192.168.220.43:3306 check port 9200 inter 2000 rise 2 fall 5
+#  server  control01 192.168.220.41:3306 check port 9220 inter 2000 rise 2 fall 5
+#  server  control02 192.168.220.42:3306 check port 9220 inter 2000 rise 2 fall 5
+#  server  control03 192.168.220.43:3306 check port 9220 inter 2000 rise 2 fall 5
 #
 # Example Usage:
 #
@@ -76,7 +76,7 @@ class galera::health_check(
   }
 
   file { "${xinetd_dir}/mysqlchk":
-    mode    => '0644',
+    mode    => '0600',
     require => File[$xinetd_dir],
     content => template("galera/mysqlchk"),
     owner   => 'root',
@@ -90,10 +90,10 @@ class galera::health_check(
     changes => [
       "ins service-name after service-name[last()]",
       "set service-name[last()] mysqlchk",
-      "set service-name[. = 'mysqlchk']/port 9200",
+      "set service-name[. = 'mysqlchk']/port 9220",
       "set service-name[. = 'mysqlchk']/protocol tcp",
     ],  
-    onlyif => "match service-name[port = '9200'] size == 0",
+    onlyif => "match service-name[port = '9220'] size == 0",
   }
 
   # Create a user for script to use for checking MySQL health status.
